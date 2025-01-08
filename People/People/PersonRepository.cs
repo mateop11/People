40a1b1d70f490dc9393aa7e5f7e1b69cfa580282
Mateo Mikael Pillajo Guijarro <mateo.pillajo@udla.edu.ec>
2025-01-07 19:27:53 -0500

    Se creo la coneccion async, se inserto un elemento en una tabla de forma asincrónica, se obtuviueron todos los elementos de una tabla de forma asincrónica y por ultimo se testeo.

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,5 +1,6 @@
 ﻿using SQLite;
 using People.Models;
+using System.Threading.Tasks;
 
 namespace People;
 
@@ -9,53 +10,53 @@ public class PersonRepository
 
     public string StatusMessage { get; set; }
 
-    private SQLiteConnection conn;
+    private SQLiteAsyncConnection conn;
 
-    private void Init()
+    private async Task Init()
     {
         if (conn != null)
             return;
 
-        conn = new SQLiteConnection(_dbPath);
-        conn.CreateTable<Person>();
+        conn = new SQLiteAsyncConnection(_dbPath);
+
+        await conn.CreateTableAsync<Person>();
     }
+
+
 
     public PersonRepository(string dbPath)
     {
         _dbPath = dbPath;                        
     }
 
-    public void AddNewPerson(string name)
-    {            
+    public async Task AddNewPerson(string name)
+    {
         int result = 0;
         try
         {
-            Init();
+            // Call Init()
+            await Init();
 
             // basic validation to ensure a name was entered
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
-            result = conn.Insert(new Person { Name = name });
-            result = 0;
+            result = await conn.InsertAsync(new Person { Name = name });
 
-            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+            StatusMessage = string.Format("{0} record(s) added [Name: {1})", result, name);
         }
         catch (Exception ex)
         {
             StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
         }
-
     }
 
-    public List<Person> GetAllPeople()
+    public async Task<List<Person>> GetAllPeople()
     {
-        // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            Init();
-            return conn.Table<Person>().ToList();
-
+            await Init();
+            return await conn.Table<Person>().ToListAsync();
         }
         catch (Exception ex)
         {

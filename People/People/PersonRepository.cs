ad712e6c2551ae87e1ee6af1cdda2363e10a45b8
Mateo Mikael Pillajo Guijarro <mateo.pillajo@udla.edu.ec>
2025-01-07 19:05:13 -0500

    Se realizo cambios en la clase person y en presonreposaitory

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,4 +1,5 @@
-﻿using People.Models;
+﻿using SQLite;
+using People.Models;
 
 namespace People;
 
@@ -8,11 +9,15 @@ public class PersonRepository
 
     public string StatusMessage { get; set; }
 
-    // TODO: Add variable for the SQLite connection
+    private SQLiteConnection conn;
 
     private void Init()
     {
-        // TODO: Add code to initialize the repository         
+        if (conn != null)
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>();
     }
 
     public PersonRepository(string dbPath)
@@ -25,13 +30,13 @@ public class PersonRepository
         int result = 0;
         try
         {
-            // TODO: Call Init()
+            Init();
 
             // basic validation to ensure a name was entered
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
-            // TODO: Insert the new person into the database
+            result = conn.Insert(new Person { Name = name });
             result = 0;
 
             StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
@@ -48,7 +53,9 @@ public class PersonRepository
         // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            
+            Init();
+            return conn.Table<Person>().ToList();
+
         }
         catch (Exception ex)
         {
